815ad77e83ced0fb835a08a7333c397a838b6b75
black_hades <2846580032@qq.com>
2024-04-20 23:47:36 +0800

    修改练习文件路径

--- practice/test.py
+++ practice/test.py
@@ -5,7 +5,7 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((1200, 800))
 screen_rect = screen.get_rect()
-vector = pygame.image.load("images/ship.bmp")
+vector = pygame.image.load("../images/ship.bmp")
 vector_rect = vector.get_rect()
 vector_rect.center = screen_rect.center
 vector_status = False

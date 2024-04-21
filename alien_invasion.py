c4eac25a4ebe6f16ddd1b29d78afaf70fe2f1f30
black_hades <2846580032@qq.com>
2024-04-21 14:26:53 +0800

    新增:外星人群(行)

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 class AlienInvasion:
@@ -23,6 +24,9 @@ class AlienInvasion:
         self.ship = Ship(self)
         # 创建存储子弹的编组
         self.bullets = pygame.sprite.Group()
+        # 创建存储外星人的编组
+        self.aliens = pygame.sprite.Group()
+        self._create_fleet()
         # 解决键盘字母输入导致程序卡死问题
         pygame.key.stop_text_input()
 
@@ -86,6 +90,22 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+    def _create_fleet(self):
+        """创建外星人群"""
+        # 创建一个外星人并计算一行可容纳多少个外星人
+        # 外星人的间距为外星人宽度
+        alien = Alien(self)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+        # 创建第一行的外星人
+        for alien_number in range(number_aliens_x):
+            # 创建一个外星人并将其加入当前行
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
+
     def _update_screen(self):
         """更新屏幕上的图像, 并切换到新屏幕"""
         # 每次循环时都能重绘屏幕
@@ -93,6 +113,7 @@ class AlienInvasion:
         self.ship.blitme()
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        self.aliens.draw(self.screen)
         # 让最近绘制的屏幕可见
         pygame.display.flip()
 
